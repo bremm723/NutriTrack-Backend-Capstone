7505bece1abe1128ba89a47c12b83c8760217176
bremm723 <bremm723@gmail.com>
2026-04-07 09:34:45 +0700

    fix prisma

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,17 +1,17 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
-// Override SSL untuk self-signed certificate
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-        checkServerIdentity: () => undefined
-    }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+    checkServerIdentity: () => undefined
+  }
 });
 
 const adapter = new PrismaPg(pool);
