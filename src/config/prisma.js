710258909225b176c5701b225b07d4add09ffced
bremm723 <bremm723@gmail.com>
2026-04-07 10:10:40 +0700

    fixing prisma

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,19 +1,27 @@
+// import "dotenv/config";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import pg from "pg";
+
+
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+// const pool = new pg.Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//     checkServerIdentity: () => undefined
+//   }
+// });
+
+// const adapter = new PrismaPg(pool);
+// const prisma = new PrismaClient({ adapter });
+
+// export default prisma;
+
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-    checkServerIdentity: () => undefined
-  }
-});
-
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
